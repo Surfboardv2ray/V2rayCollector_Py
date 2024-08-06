82ee38d626f3d269c259b64306e0d92ee7f63c69
SurfboardV2ray <125461738+Surfboardv2ray@users.noreply.github.com>
2024-08-07 01:11:22 +0330

    Update main.py

--- main.py
+++ main.py
@@ -110,6 +110,7 @@ def create_sub_section():
 if __name__ == "__main__":
     telegram_urls = [
         "https://t.me/s/v2ray_configs_pool",
+        "https://t.me/s/ConfigsHUB2",
     ]
 
     all_v2ray_configs = []
